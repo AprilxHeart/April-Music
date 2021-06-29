c6116324a233fa3929d8c32a20cb5aeaf315eae7
AprilxHeart <86656977+AprilxHeart@users.noreply.github.com>
2021-06-29 16:45:54 +0700

    First

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODU5MzQ2NjUwMjM3ODk0NjU3.YNrW7w.2l4J1J3CG2G_4mXXoqh_-8BAZNc',
+        prefix: '!',
+        activity: 'Music For U',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
